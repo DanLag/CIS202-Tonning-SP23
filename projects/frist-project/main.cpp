0acf38d1151ddd5c2140cdfa35d7ab6d94e4af52
DanLag <47798282+DanLag@users.noreply.github.com>
2023-06-07 22:35:07 -0700

    Update main.cpp

--- projects/frist-project/main.cpp
+++ projects/frist-project/main.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include "tree.h"
 
-using namespace std;
 
-int main() {
-    cout<< "Hello" << endl;
+int main()
+{
+   Tree t1("Anne");
+   Tree t2("Peter");
+   t1.add_subtree(t2);
+   Tree t3("Zara");
+   t1.add_subtree(t3);
+   Tree t4("Savannah");
+   t2.add_subtree(t4);
+   std::cout << "Size: " << t1.size() << std::endl;
 
-    return 0;
+   t1.print();   // this will recursively print the tree.
+
+   return 0;
 }
